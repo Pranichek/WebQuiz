da1eb6aa11299d026b71852fdd95e23fcab90bf4
admin <vovagrinchenko19@gmail.com>
2025-05-16 00:46:59 +0300

    add try function

--- userprofile/views.py
+++ userprofile/views.py
@@ -61,83 +61,87 @@ def render_profile():
     
     
 def render_edit_avatar():
-    user = flask_login.current_user
-    show = ['']
-    if flask.request.method == "POST":
-        check_form = flask.request.form.get("check_form")
-        if check_form == "load_image":
-            if 'file' not in flask.request.files:
-                return flask.redirect("/")
-            
-            file = flask.request.files["file"]
-            if not os.path.exists(os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(flask_login.current_user.email), "cash"))):
-                os.mkdir(path= os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(flask_login.current_user.email), "cash")))
-            else:
-                if len(os.listdir(os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(flask_login.current_user.email), "cash")))) > 0:
-                    for file_dir in os.listdir(os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(flask_login.current_user.email), "cash"))):
-                        os.remove(path = os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(flask_login.current_user.email), "cash", file_dir)))
-
-            path_to_avatar = os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(flask_login.current_user.email), "cash", str(file.filename)))
-            if not os.path.exists(path = path_to_avatar):
-                file.save(path_to_avatar)
-
-            flask.session["cash_image"] =  str(file.filename)
-            show[0] = 'show'
-            # flask_login.current_user.name_avatar = str(file.filename)
-            # DATABASE.session.commit()
-        elif check_form == "apply_image":
-            try:
-                show[0] = ''
-                flask_login.current_user.name_avatar = flask.session["cash_image"]
-                DATABASE.session.commit()
-
-                img = PIL.Image.open(fp = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email), "cash", str(flask.session["cash_image"]))))
-                # save a image using extension
-
-                img = img.save(fp = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email) , str(flask.session["cash_image"]))))
-                os.remove(path = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email), "cash", str(flask.session["cash_image"]))))
+    try:
+        user = flask_login.current_user
+        show = ['']
+        if flask.request.method == "POST":
+            check_form = flask.request.form.get("check_form")
+            if check_form == "load_image":
+                if 'file' not in flask.request.files:
+                    return flask.redirect("/")
                 
-            except Exception as error:
-                print(error)
-        elif check_form == "defaul_avatar":
-            number_avatar = flask.request.form.get("data_avatar")
+                file = flask.request.files["file"]
+                user = User.query.get(flask_login.current_user.id)
+                if not os.path.exists(os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(user.email), "cash"))):
+                    os.mkdir(path= os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(user.email), "cash")))
+                else:
+                    if len(os.listdir(os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(user.email), "cash")))) > 0:
+                        for file_dir in os.listdir(os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(user.email), "cash"))):
+                            os.remove(path = os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(user.email), "cash", file_dir)))
 
-            name_avatar = ''
+                path_to_avatar = os.path.abspath(os.path.join(__file__, "..", "static", "images", 'edit_avatar', str(user.email), "cash", str(file.filename)))
+                if not os.path.exists(path = path_to_avatar):
+                    file.save(path_to_avatar)
 
-            if number_avatar == "1":
-                name_avatar = "default_avatar.png"
-            elif number_avatar == "2":
-                name_avatar = "default_picture2.png"
-            elif number_avatar == "3":
-                name_avatar = "default_picture3.png"
-            elif number_avatar == "4":
-                name_avatar = "default_picture4.png"
-            elif number_avatar == "5":
-                name_avatar = "default_picture5.png"
-        
-            default_img = PIL.Image.open(fp = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", name_avatar)))
-            # save a image using extension
-            if not os.path.exists(path = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email) , name_avatar))):
-                default_img = default_img.save(fp=os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email) , name_avatar)))
+                flask.session["cash_image"] =  str(file.filename)
+                show[0] = 'show'
+                # flask_login.current_user.name_avatar = str(file.filename)
+                # DATABASE.session.commit()
+            elif check_form == "apply_image":
+                try:
+                    show[0] = ''
+                    flask_login.current_user.name_avatar = flask.session["cash_image"]
+                    DATABASE.session.commit()
 
-            flask_login.current_user.name_avatar = name_avatar
-            DATABASE.session.commit()
-        elif check_form == "del_image":
-            default_avatars = ["default_avatar.png", "default_picture2.png", "default_picture3.png", "default_picture4.png","default_picture5.png"]
-            if str(flask_login.current_user.name_avatar) not in default_avatars:
-                flask_login.current_user.name_avatar = str(random.choice(default_avatars))
-                DATABASE.session.commit()
-        
-        elif check_form == "back":
-            show[0] = ''
+                    img = PIL.Image.open(fp = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email), "cash", str(flask.session["cash_image"]))))
+                    # save a image using extension
 
+                    img = img.save(fp = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email) , str(flask.session["cash_image"]))))
+                    os.remove(path = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email), "cash", str(flask.session["cash_image"]))))
+                    
+                except Exception as error:
+                    print(error)
+            elif check_form == "defaul_avatar":
+                number_avatar = flask.request.form.get("data_avatar")
+
+                name_avatar = ''
+
+                if number_avatar == "1":
+                    name_avatar = "default_avatar.png"
+                elif number_avatar == "2":
+                    name_avatar = "default_picture2.png"
+                elif number_avatar == "3":
+                    name_avatar = "default_picture3.png"
+                elif number_avatar == "4":
+                    name_avatar = "default_picture4.png"
+                elif number_avatar == "5":
+                    name_avatar = "default_picture5.png"
             
-    if flask_login.current_user.is_authenticated:
-        return flask.render_template(
-            template_name_or_list = "edit_avatar.html",
-            user = user,
-            show = show[0],
-            cash_image = str(flask.session["cash_image"] if 'cash_image' in flask.session else "Nothing")
-        )
-    else:
+                default_img = PIL.Image.open(fp = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", name_avatar)))
+                # save a image using extension
+                if not os.path.exists(path = os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email) , name_avatar))):
+                    default_img = default_img.save(fp=os.path.abspath(os.path.join(__file__, "..", "..", "userprofile", "static", "images", "edit_avatar", str(flask_login.current_user.email) , name_avatar)))
+
+                flask_login.current_user.name_avatar = name_avatar
+                DATABASE.session.commit()
+            elif check_form == "del_image":
+                default_avatars = ["default_avatar.png", "default_picture2.png", "default_picture3.png", "default_picture4.png","default_picture5.png"]
+                if str(flask_login.current_user.name_avatar) not in default_avatars:
+                    flask_login.current_user.name_avatar = str(random.choice(default_avatars))
+                    DATABASE.session.commit()
+            
+            elif check_form == "back":
+                show[0] = ''
+
+                
+        if flask_login.current_user.is_authenticated:
+            return flask.render_template(
+                template_name_or_list = "edit_avatar.html",
+                user = user,
+                show = show[0],
+                cash_image = str(flask.session["cash_image"] if 'cash_image' in flask.session else "Nothing")
+            )
+        else:
+            return flask.redirect("/")
+    except Exception as error:
         return flask.redirect("/")
