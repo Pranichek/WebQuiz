066f12a8cd43bd8141eb565371af8974f495f9be
BoiarkinaOryna <boyarkina.ar@gmail.com>
2025-06-01 14:05:56 +0300

    test creation validation

--- quiz/static/js/createTest.js
+++ quiz/static/js/createTest.js
@@ -1,3 +1,64 @@
-const category = document.getElementById("time");
-const testTitle = document.querySelector("test-title-input");
+// const category = document.getElementById("time");
+// const testTitle = document.querySelector(".test-title-input");
+const createTestButtons = document.getElementsByClassName("add-test");
 
+// function dataValidation(){
+//     // console.log("Value = ", testTitle.value);
+//     if (document.cookie.match("inputname") && document.cookie.split("inputname=")[1].split(";")[0] != ""){
+//         console.log(":", document.cookie.match("category"));
+//         if (document.cookie.match("category")){
+//         //     console.log("category.textContent =", category.textContent)
+//             for (let createTestButton of createTestButtons){
+//                 createTestButton.classList.remove("grey");
+//                 createTestButton.type = "submit";
+//             }
+//         }
+//     }
+// }
+
+function dataValidation() {
+    const hasName = document.cookie.match("inputname") && document.cookie.split("inputname=")[1].split(";")[0] != "";
+    const hasCategory = document.cookie.match("category");
+
+    for (let createTestButton of createTestButtons) {
+        if (hasName && hasCategory) {
+            createTestButton.classList.remove("grey");
+            createTestButton.type = "submit";
+        } else {
+            createTestButton.classList.add("grey");
+            createTestButton.type = "button";
+        }
+    }
+}
+
+function buttonColorChanging(){
+    for (let createTestButton of createTestButtons){
+        console.log("createTestButton.type =", createTestButton.type)
+        if (createTestButton.type == "button"){
+            console.log("change button color");
+            createTestButton.classList.add("grey");
+            console.log(createTestButton.classList)
+        }
+    }
+}
+
+document.addEventListener("click", ()=>{
+    dataValidation();
+})
+document.addEventListener("keydown", ()=>{
+    dataValidation();
+})
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("content loaded");
+    dataValidation();
+})
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    buttonColorChanging();
+})
+document.addEventListener("click", ()=>{
+    buttonColorChanging();
+})
+document.addEventListener("keydown", ()=>{
+    buttonColorChanging();
+})
