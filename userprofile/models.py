895392e308fabf96c9469107fd12ae63501f1a0c
dianaaao <ovcarovadiana035@gmail.com>
2025-06-07 14:33:51 +0300

    fixed style and add images

--- userprofile/models.py
+++ userprofile/models.py
@@ -10,6 +10,9 @@ class DataUser(DATABASE.Model):
     # айді останніх пройденів тестів
     last_passed = DATABASE.Column(DATABASE.String, default = '')
 
+    # кількість балів користувача
+    # amount_money = DATABASE.Column(DATABASE.Integer, default = 300)
+
     # зв'язок із моделлю користувача one-to-one
     user = DATABASE.relationship("User", back_populates="user_profile")
     user_id = DATABASE.Column(DATABASE.Integer, DATABASE.ForeignKey('user.id'), unique=True)
