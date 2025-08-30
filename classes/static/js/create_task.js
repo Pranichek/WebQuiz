019bae6223f9bb100a1cc78aca56585b0172d4dd
dianaaao <ovcarovadiana035@gmail.com>
2025-08-30 12:04:05 +0300

    add modal windows

--- classes/static/js/create_task.js
+++ classes/static/js/create_task.js
@@ -1,12 +1,32 @@
 let buttonPlus = document.querySelector(".add_task")
 let divChoice = document.querySelector(".choice")
 let divAnnouncement = document.querySelector(".announcement")
+let divTestTask = document.querySelector(".test_task")
+let divNextTestTask = document.querySelector(".next_test_task")
 let buttonAnnouncement = document.querySelector(".announcement-btn")
+let buttonTestTask = document.querySelector(".test_task-btn")
+let buttonChooseTest = document.querySelector(".choose-test")
 let buttonCreateTask = document.querySelector(".create_task")
+// let buttonCreateTestTask = document.querySelector(".create_test_task")
 const socket = io()
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 
+
+function showWithFade(el) {
+  el.style.display = "flex";           
+  el.classList.remove("is-open");      
+  requestAnimationFrame(() => {        
+    el.classList.add("is-open");
+  });
+}
+
+function hideWithFade(el) {
+  el.classList.remove("is-open");   
+  setTimeout(() => {                   
+    el.style.display = "none";
+  }, 280);
+}
 
 
 buttonPlus.addEventListener(
@@ -14,6 +34,9 @@ buttonPlus.addEventListener(
     () => {
         divChoice.style.display = "flex"
         divAnnouncement.style.display = "none"
+
+        showWithFade(divChoice);
+        hideWithFade(divAnnouncement);
     }
 )
 buttonAnnouncement.addEventListener(
@@ -21,6 +44,30 @@ buttonAnnouncement.addEventListener(
     () => {
         divChoice.style.display = "none"
         divAnnouncement.style.display = "flex"
+
+        hideWithFade(divChoice);
+        showWithFade(divAnnouncement);
+    }
+)
+buttonTestTask.addEventListener(
+    'click',
+    () => {
+        divChoice.style.display = "none"
+        divTestTask.style.display = "flex"
+
+        hideWithFade(divChoice);
+        showWithFade(divTestTask);
+    }
+)
+
+buttonChooseTest.addEventListener(
+    'click',
+    () => {
+        divTestTask.style.display = "none"
+        divNextTestTask.style.display = "flex"
+
+        hideWithFade(divTestTask);
+        showWithFade(divNextTestTask);
     }
 )
 
@@ -40,3 +87,15 @@ buttonCreateTask.addEventListener(
         location.reload()    
     }
 )
+// buttonCreateTestTask.addEventListener(
+//     'click',
+//     () => {
+//         socket.emit(
+//             'create_test_task',
+//             {
+//                 information: document.querySelector(".next_test_task_info").value,
+//                 class_id: urlParams.get('class_id')
+//             }
+//         )
+//     }
+// )
